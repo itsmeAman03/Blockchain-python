a9649a63418f5e88d655bea681e8e678a46f3c7b
itsmeAman03 <aman.kr,ak03@gmail.com>
2022-06-05 16:22:55 +0530

    added test_genesis function for testing data of GENESIS_DATA

--- backend/tests/blockchain/test+block.py
+++ backend/tests/blockchain/test+block.py
@@ -1,4 +1,4 @@
-from backend.blockchain.block import Block
+from backend.blockchain.block import Block,GENESIS_DATA
 def test_mine_block():
     last_block = Block.genesis()
     data = 'test-data'
@@ -6,4 +6,16 @@ def test_mine_block():
 
     assert isinstance(block,Block)
     assert block.data=joined_data
-    assert block.last_block=last_block.hash
+    assert block.last_block=last_block.hash'
+
+def test_genesis():
+    genesis=Block.genesis()
+    assert isinstance(genesis,Block)
+    
+    assert genesis.timestamp== GENESIS_DATA['timestamp']
+    assert genesis.last_hash== GENESIS_DATA['last_hash']
+    assert genesis.hash== GENESIS_DATA['hash']
+    assert genesis.data== GENESIS_DATA['data']
+    
+    for key,value in GENESIS_DATA.items()
+     assert getattr(genesis,key)==value
